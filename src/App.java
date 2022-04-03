98d3f8a8705e2cde8403e39fa4bcb11bf31a135e
Yevhen Kudiukin <kudyakin@gmail.com>
2022-04-03 16:41:55 +0300

    4 commit

--- src/App.java
+++ src/App.java
@@ -684,16 +684,111 @@ public class App {
 //    }
 
 
+//    public static void main(String[] args) {
+//        if (args.length > 1) {
+//            if (args.length > 2) {
+//                if (args.length > 3) {
+//                }
+//            }
+//        }
+//    }
+
+
+//    public static void main(String[] args) {
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 0; j < 10; i++) {
+//                for (int k = 0; k < 10; k++) {
+//
+//                }
+//            }
+//        }
+//    }
+
+
+//    public static void main(String[] args) {
+//        try {
+//            System.err.print(" 0");
+//            try {
+//                System.err.print(" 1");
+//                // НИЧЕГО
+//                System.err.print(" 2");
+//            } catch (RuntimeException e) {
+//                System.err.print(" 3"); // НЕ заходим - нет исключения
+//            } finally {
+//                System.err.print(" 4"); // заходим всегда
+//            }
+//            System.err.print(" 5");     // заходим - выполнение в норме
+//        } catch (Exception e) {
+//            System.err.print(" 6");     // НЕ заходим - нет исключения
+//        } finally {
+//            System.err.print(" 7");     // заходим всегда
+//        }
+//        System.err.print(" 8");         // заходим - выполнение в норме
+//    }
+
+
+//    public static void main(String[] args) {
+//        try {
+//            System.err.print(" 0");
+//            try {
+//                System.err.print(" 1");
+//                if (true) {throw new RuntimeException();}
+//                System.err.print(" 2");
+//            } catch (RuntimeException e) {
+//                System.err.print(" 3"); // ЗАХОДИМ - есть исключение
+//            } finally {
+//                System.err.print(" 4"); // заходим всегда
+//            }
+//            System.err.print(" 5");     // заходим - выполнение УЖЕ в норме
+//        } catch (Exception e) {
+//            System.err.print(" 6");     // не заходим - нет исключения, УЖЕ перехвачено
+//        } finally {
+//            System.err.print(" 7");     // заходим всегда
+//        }
+//        System.err.print(" 8");         // заходим - выполнение УЖЕ в норме
+//    }
+
+
+//    public static void main(String[] args) {
+//        try {
+//            System.err.print(" 0");
+//            try {
+//                System.err.print(" 1");
+//                if (true) {throw new Exception();}
+//                System.err.print(" 2");
+//            } catch (RuntimeException e) {
+//                System.err.print(" 3"); // НЕ заходим - есть исключение, но НЕПОДХОДЯЩЕГО ТИПА
+//            } finally {
+//                System.err.print(" 4"); // заходим всегда
+//            }
+//            System.err.print(" 5");     // не заходим - выполнение НЕ в норме
+//        } catch (Exception e) {
+//            System.err.print(" 6");     // ЗАХОДИМ - есть подходящее исключение
+//        } finally {
+//            System.err.print(" 7");     // заходим всегда
+//        }
+//        System.err.print(" 8");         // заходим - выполнение УЖЕ в норме
+//    }
+
+
     public static void main(String[] args) {
-        if (args.length > 1) {
-            if (args.length > 2) {
-                if (args.length > 3) {
-                }
+        try {
+            System.err.print(" 0");
+            try {
+                System.err.print(" 1");
+                if (true) {throw new Error();}
+                System.err.print(" 2");
+            } catch (RuntimeException e) {
+                System.err.print(" 3"); // НЕ заходим - есть исключение, но НЕПОДХОДЯЩЕГО ТИПА
+            } finally {
+                System.err.print(" 4"); // заходим всегда
             }
+            System.err.print(" 5");     // НЕ заходим - выполнение НЕ в норме
+        } catch (Exception e) {
+            System.err.print(" 6");     // не заходим - есть исключение, но НЕПОДХОДЯЩЕГО ТИПА
+        } finally {
+            System.err.print(" 7");     // заходим всегда
         }
+        System.err.print(" 8");         // не заходим - выполнение НЕ в норме
     }
-
-
-
-
 }
